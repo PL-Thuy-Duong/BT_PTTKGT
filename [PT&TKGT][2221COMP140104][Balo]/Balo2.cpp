146b903c1bd2fa1e4a353ec531600ed94a14f8d6
PL-Thuy-Duong <113046139+PL-Thuy-Duong@users.noreply.github.com>
2023-06-18 02:51:49 +0700

    Update Balo2.cpp

--- [PT&TKGT][2221COMP140104][Balo]/Balo2.cpp
+++ [PT&TKGT][2221COMP140104][Balo]/Balo2.cpp
@@ -10,29 +10,24 @@ int n, w;
 int a[100],b[100], f[100][101]; // mảng f[i][j] sẽ chứa giá trị tối ưu của balo có trọng lượng tối đa j ở lượt chọn vật thứ i
 int p[100], o = 0;
 int main() {
-    cout << "Moi nhap so luong mon do: \n";
-    cin >> n;
-    cout << "Moi nhap khoi luong toi da cua balo: \n";
-    cin >> w;
-    cout << "Moi nhap khoi luong và gia tri tung mon do: \n";
-    for (int i = 1; i <= n; ++i)
+    cin >> n >> w; // nhập tổng số món và số kg balo tối đa
+    for (int i = 1; i <= n; ++i) // nhập số món đồ và giá trị món đó
         cin >> a[i] >> b[i];
 
-    for (int i = 1; i <= n; ++i) // xét lần lượt các món đồ
+    for (int i = 1; i <= n; i++) // xét lần lượt các món đồ
     {
-        for (int v = 1; v <= w; ++v) // xét các ba lo có khối lượng tối đa từ bé đến lớn
+        for (int j = 1; j <= w; v++) // xét các ba lo có khối lượng tối đa từ bé đến lớn
         {
-            if (v >= a[i]) // nếu vật thứ i có thể bỏ được vào balo
+            if (j >= a[i]) // nếu vật thứ i có thể bỏ được vào balo
             {
-                f[i][v] = max(f[i - 1][v - a[i]] + b[i], f[i - 1][v]);
-                // khối lượng tốt nhất của ba lô có khối lượng tối đa v có thể mang được hoặc bằng lựa chọn trước đó (f[i - 1][v])
-                // hoặc bằng lựa chọn tối ưu ở ba lô có khối lượng v - a[i] cộng với khối lượng a[i] đang xét
+                f[i][j] = max(b[i] + f[i - 1][j - a[i]], f[i - 1][j]);
+                // khối lượng tốt nhất của ba lô có khối lượng tối đa j có thể mang được hoặc bằng lựa chọn trước đó (f[i - 1][j])
+                // hoặc bằng lựa chọn tối ưu ở ba lô có khối lượng j - a[i] cộng với khối lượng a[i] đang xét
             }
-            else f[i][v] = f[i - 1][v]; // nếu không thì sẽ bằng lựa chọn tối ưu trc đó.
+            else f[i][j] = f[i - 1][j]; // nếu không thì sẽ bằng lựa chọn tối ưu trc đó.
         }
     }
-    cout << "\nGia tri toi uu ma balo mang duoc: \n" << f[n][w] << '\n';
-    cout << "Danh sach thu tu cac mon do duoc chon: \n";
+    cout << f[n][w];
     for (int i = n; i >= 0; --i) // hàm truy vết nếu
     {
         if (f[i][w] != f[i - 1][w]) // nếu bằng nhau thì chứng tỏ vật a[i] không được chọn và ngược lại
@@ -42,7 +37,6 @@ int main() {
         }
     }
     for (int i = o - 1; i >= 0; --i)
-        cout << p[i] << " ";
-
+        cout << p[i] << " ";// xuất ra vị trí món đồ đc chọn
     return 0;
 }
