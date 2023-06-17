db5e632f3c012c4e177e98e6fc075fd8a9f0165a
PL-Thuy-Duong <113046139+PL-Thuy-Duong@users.noreply.github.com>
2023-06-18 02:46:07 +0700

    Update Balo1.cpp

--- [PT&TKGT][2221COMP140104][Balo]/Balo1.cpp
+++ [PT&TKGT][2221COMP140104][Balo]/Balo1.cpp
@@ -1,53 +1,30 @@
 #include <iostream>
-#include <vector>
-
 using namespace std;
-
-vector<int> knapsack(int n, int w, const vector<int>& weights) {
-    vector<vector<int>> dp(n + 1, vector<int>(w + 1, 0));
-    
-    // Xây dựng bảng dp
+int main(){
+int n, a[n+1] = {0}, w;
+int f[100][100];
+    cin>>n>>w;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+    }
+    // Xây dựng bảng f
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= w; j++) {
-            if (weights[i - 1] <= j) {
-                dp[i][j] = max(dp[i - 1][j], weights[i - 1] + dp[i - 1][j - weights[i - 1]]);
+            if (a[i]<=j) {
+                f[i][j] = max(f[i - 1][j],f[i - 1][j-a[i]] +a[i]);// quy hoạch động
             } else {
-                dp[i][j] = dp[i - 1][j];
+                f[i][j] = f[i - 1][j];
             }
         }
     }
-    
-    // Truy vết để lấy các món hàng được chọn
-    int i = n, j = w;
-    vector<int> selectedItems;
-    while (i > 0 && j > 0) {
-        if (dp[i][j] != dp[i - 1][j]) {
-            selectedItems.push_back(i - 1);
-            j -= weights[i - 1];
+    //xuất ra bảng f
+    for(int i=1;i<=n;i++) {
+        for (int j = 0; j <= w; j++) {
+            cout<< f[i][j]<<" ";
         }
-        i--;
-    }
-    
-    return selectedItems;
-}
-
-int main() {
-    int n, w;
-    cin >> n >> w;
-    
-    vector<int> weights(n);
-    for (int i = 0; i < n; i++) {
-        cin >> weights[i];
-    }
-    
-    vector<int> selectedItems = knapsack(n, w, weights);
-    
-    cout << selectedItems.size() << endl;
-  
-    for (int i = selectedItems.size() - 1; i >= 0; i--) {
-        cout << selectedItems[i] << " ";
-    }
-    cout << endl;
-    
+        cout<<endl;
+    }   
+    court<<f[n][w];//xuất khối lượng tối đa
     return 0;
 }
